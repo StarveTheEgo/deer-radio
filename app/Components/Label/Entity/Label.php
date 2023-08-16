b84b36eb0a64d2349fdd2840bdf6feceff9e19d8
StarveTheEgo <alone_monax@xakep.ru>
2023-08-16 21:37:27 +0300

    Added LabelLinks collection to Label

--- app/Components/Label/Entity/Label.php
+++ app/Components/Label/Entity/Label.php
@@ -7,8 +7,10 @@ namespace App\Components\Label\Entity;
 use App\Components\DoctrineOrchid\AbstractDomainObject;
 use App\Components\DoctrineOrchid\TimestampableEntityTrait;
 use App\Components\DoctrineOrchid\TimestampableInterface;
+use App\Components\LabelLink\Entity\LabelLink;
 use App\Components\Song\Entity\Song;
 use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
@@ -27,10 +29,14 @@ class Label extends AbstractDomainObject implements TimestampableInterface
     protected string $name;
 
     #[ORM\OneToMany(mappedBy: 'label', targetEntity: Song::class)]
-    protected ArrayCollection $songs;
+    protected Collection $songs;
+
+    #[ORM\OneToMany(mappedBy: 'label', targetEntity: LabelLink::class)]
+    protected Collection $links;
 
     public function __construct() {
         $this->songs = new ArrayCollection();
+        $this->links = new ArrayCollection();
     }
 
     /**
@@ -57,5 +63,13 @@ class Label extends AbstractDomainObject implements TimestampableInterface
     {
         $this->name = $name;
         return $this;
+    }
+
+    /**
+     * @return LabelLink[]
+     */
+    public function getLinks(): array
+    {
+        return $this->links->toArray();
     }
 }
