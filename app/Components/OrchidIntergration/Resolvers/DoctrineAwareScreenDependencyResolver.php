7035ab3c8814e82e06c50ecefddcbcaf26224ae7
StarveTheEgo <alone_monax@xakep.ru>
2024-01-01 07:22:36 +0300

    Added Screen's route parameters resolver that supports Doctrine entities

---
+++ app/Components/OrchidIntergration/Resolvers/DoctrineAwareScreenDependencyResolver.php
@@ -0,0 +1,155 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Components\OrchidIntergration\Resolvers;
+
+use App\Components\DoctrineOrchid\AbstractDomainObject;
+use App\Components\DoctrineOrchid\Exceptions\ObjectNotFoundException;
+use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ObjectRepository;
+use Illuminate\Contracts\Routing\UrlRoutable as LaravelUrlRoutable;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\Route;
+use LaravelDoctrine\ORM\Contracts\UrlRoutable as DoctrineUrlRoutable;
+use Orchid\Screen\Resolvers\ScreenDependencyResolver;
+use Orchid\Screen\Screen;
+use ReflectionClass;
+use ReflectionException;
+use ReflectionParameter;
+use Throwable;
+
+/**
+ * "Patched" version of ScreenDependencyResolver to support Doctrine route bindings
+ * @see ScreenDependencyResolver
+ */
+class DoctrineAwareScreenDependencyResolver
+{
+    /**
+     * @var ManagerRegistry
+     */
+    protected ManagerRegistry $entityManagerRegistry;
+
+    /**
+     * @param ManagerRegistry $registry
+     */
+    public function __construct(ManagerRegistry $registry)
+    {
+        $this->entityManagerRegistry = $registry;
+    }
+
+    /**
+     * Resolves arguments for specified method of the screen
+     * @param Screen $screen
+     * @param string $method
+     * @param array<string, mixed> $httpQueryArguments
+     *
+     * @throws ReflectionException
+     *
+     * @return array<string, mixed>
+     */
+    public function resolveScreen(Screen $screen, string $method, array $httpQueryArguments = []): array
+    {
+        $parameters = (new ReflectionClass($screen))->getMethod($method)->getParameters();
+
+        $argumentValues = collect($httpQueryArguments);
+        $currentRoute = Route::current();
+
+        return collect($parameters)
+            ->map(function (ReflectionParameter $parameter) use ($argumentValues, $currentRoute) {
+                $currentArgumentValue = $argumentValues->shift();
+                $resolvedValue = $this->resolveValue($parameter, $currentArgumentValue);
+
+                $currentRoute?->setParameter($parameter->getName(), $resolvedValue);
+
+                return $resolvedValue;
+            })
+            ->all();
+    }
+
+    /**
+     * Resolves value for specified parameter
+     * @param ReflectionParameter $parameter
+     * @param mixed $parameterValue
+     * @return mixed
+     * @throws Throwable
+     */
+    private function resolveValue(ReflectionParameter $parameter, mixed $parameterValue): mixed
+    {
+        $parameterClassName = $this->getClassName($parameter);
+
+        if ($parameterClassName === null) {
+            // built-in / non-specified parameter type
+            return $parameterValue;
+        }
+
+        $parameterClass = new ReflectionClass($parameterClassName);
+
+        if ($parameterClass->isSubclassOf(AbstractDomainObject::class)) {
+            // is a Doctrine object
+            $repository = $this->entityManagerRegistry->getRepository($parameterClassName);
+            $resolvedObject = $this->resolveDoctrineEntity($repository, $parameterClass, $parameterValue);
+
+            throw_if(
+                $resolvedObject === null && !$parameter->isDefaultValueAvailable(),
+                (new ObjectNotFoundException())->setObjectInfo($parameterClass->getName(), [$parameterValue])
+            );
+        } else {
+            // possible is a Laravel model
+            $resolvedObject = resolve($parameterClass);
+            if (!is_a($resolvedObject, LaravelUrlRoutable::class)) {
+                // is not a Laravel model
+                return $resolvedObject;
+            }
+
+            if ($parameterValue === null) {
+                // value is null, - returning uninitialized model
+                return $resolvedObject;
+            }
+
+            $resolvedObject = $resolvedObject->resolveRouteBinding($parameterValue);
+            throw_if(
+                $resolvedObject === null && !$parameter->isDefaultValueAvailable(),
+                (new ModelNotFoundException())->setModel($parameterClass, [$parameterValue])
+            );
+        }
+
+        return $resolvedObject;
+    }
+
+    /**
+     * Resolved specified Doctrine entity
+     * @param ObjectRepository $repository
+     * @param ReflectionClass $parameterClass
+     * @param $parameterValue
+     * @return object|null
+     */
+    private function resolveDoctrineEntity(ObjectRepository $repository, ReflectionClass $parameterClass, $parameterValue) : ?object
+    {
+        if ($parameterClass->implementsInterface(DoctrineUrlRoutable::class)) {
+            $name = call_user_func([$parameterClass->getName(), 'getRouteKeyName']);
+
+            $entity = $repository->findOneBy([
+                $name => $parameterValue
+            ]);
+        } else {
+            $entity = $repository->find($parameterValue);
+        }
+
+        return $entity;
+    }
+
+    /**
+     * Fetches class name of parameter, if its class name is specified
+     * @param ReflectionParameter $parameter
+     * @return string|null
+     */
+    private function getClassName(ReflectionParameter $parameter): ?string
+    {
+        if (($type = $parameter->getType()) && $type instanceof \ReflectionNamedType && !$type->isBuiltin()) {
+            return $type->getName();
+        }
+
+        return null;
+    }
+}
--- app/Orchid/Screens/AbstractScreen.php
+++ app/Orchid/Screens/AbstractScreen.php
@@ -4,13 +4,14 @@ declare(strict_types=1);
 
 namespace App\Orchid\Screens;
 
+use App\Components\OrchidIntergration\Resolvers\DoctrineAwareScreenDependencyResolver;
+use Illuminate\Contracts\Container\BindingResolutionException;
 use Orchid\Screen\Screen;
+use ReflectionException;
 
 abstract class AbstractScreen extends Screen
 {
     private const DEFAULT_PER_PAGE = 25;
-
-    abstract public static function getIcon(): string;
 
     abstract public static function getRoute(): string;
 
@@ -34,6 +35,22 @@ abstract class AbstractScreen extends Screen
     public function permission(): ?iterable
     {
         return static::getPermissions();
+    }
+
+    /**
+     * @param string $method
+     * @param array<string, mixed>  $httpQueryArguments
+     *
+     * @throws BindingResolutionException
+     * @throws ReflectionException
+     *
+     * @return array<string, mixed>
+     */
+    protected function resolveDependencies(string $method, array $httpQueryArguments = []): array
+    {
+        /** @var DoctrineAwareScreenDependencyResolver $resolver */
+        $resolver = app()->make(DoctrineAwareScreenDependencyResolver::class);
+        return $resolver->resolveScreen($this, $method, $httpQueryArguments);
     }
 
     /**
