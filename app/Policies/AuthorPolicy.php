d8471ea82e92aaf2672bc18d35beaa8abf892894
StarveTheEgo <alone_monax@xakep.ru>
2024-01-02 06:11:41 +0300

    PHPStan fixes: AuthorPolicy

--- app/Policies/AuthorPolicy.php
+++ app/Policies/AuthorPolicy.php
@@ -6,34 +6,75 @@ use App\Models\Author;
 use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
-class AuthorPolicy {
+class AuthorPolicy
+{
     use HandlesAuthorization;
 
-    public function viewAny(User $user) {
+    /**
+     * @param User $user
+     * @return bool
+     */
+    public function viewAny(User $user): bool
+    {
         return true;
     }
 
-    public function view(User $user, Author $author) {
+    /**
+     * @param User $user
+     * @param Author $author
+     * @return bool
+     */
+    public function view(User $user, Author $author): bool
+    {
         return true;
     }
 
-    public function create(User $user) {
+    /**
+     * @param User $user
+     * @return bool
+     */
+    public function create(User $user): bool
+    {
         return true;
     }
 
-    public function update(User $user, Author $author) {
+    /**
+     * @param User $user
+     * @param Author $author
+     * @return bool
+     */
+    public function update(User $user, Author $author): bool
+    {
         return true;
     }
 
-    public function delete(User $user, Author $author) {
+    /**
+     * @param User $user
+     * @param Author $author
+     * @return bool
+     */
+    public function delete(User $user, Author $author): bool
+    {
         return true;
     }
 
-    public function restore(User $user, Author $author) {
+    /**
+     * @param User $user
+     * @param Author $author
+     * @return bool
+     */
+    public function restore(User $user, Author $author)
+    {
         return true;
     }
 
-    public function forceDelete(User $user, Author $author) {
+    /**
+     * @param User $user
+     * @param Author $author
+     * @return bool
+     */
+    public function forceDelete(User $user, Author $author)
+    {
         return true;
     }
 }
