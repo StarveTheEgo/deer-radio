59fe115f772aaf0c533082224c4aff6f2178cb93
StarveTheEgo <alone_monax@xakep.ru>
2024-01-03 02:43:56 +0300

    Added temporary factory for a Code field with JSON language (fixes vendor's issue)

---
+++ app/Components/OrchidIntergration/Factory/JsonFieldFactory.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Components\OrchidIntergration\Factory;
+
+use Orchid\Screen\Fields\Code;
+
+class JsonFieldFactory
+{
+    /**
+     * @param string|null $name
+     * @return Code
+     */
+    public static function make(?string $name = null) : Code
+    {
+        $field = Code::make($name) // Code
+            ->language(Code::JS); // vendor's implementation 'json' is broken, we so are using similar syntax highlighter here
+
+        $field->addBeforeRender(function () use ($field) {
+            $value = $field->get('value');
+
+            if (!is_scalar($value) && $value !== null) {
+                $value = json_encode($value, flags: JSON_THROW_ON_ERROR | JSON_PRETTY_PRINT);
+            }
+
+            $field->set('value', $value);
+        });
+
+        return $field;
+    }
+}
