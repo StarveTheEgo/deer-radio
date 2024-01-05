e3a4658c2529c5160756cc8ae269e30777009483
StarveTheEgo <alone_monax@xakep.ru>
2024-01-06 01:59:41 +0300

    Added CliCommands Controller; Registered commands required to accept requests from liquidsoap

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+declare(strict_types=1);
+
+use App\Http\Controllers\Api\Liquidsoap\CliCommandController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +20,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::controller(CliCommandController::class)
+    ->prefix('liquidsoap')
+    ->group(function () {
+        Route::get('/current-deer-image', 'getCurrentDeerImage');
+        Route::get('/update-deer-image', 'updateDeerImage');
+
+        Route::get('/next-song/{mode?}', 'getNextSong');
+        Route::get('/update-now-playing/{songId}', 'updateNowPlaying');
+
+        Route::get('/notify-stream-chat', 'notifyStreamChat');
+    });
