c3f41cba302abe7ead4d0dbe493fc04a5d0274b4
StarveTheEgo <alone_monax@xakep.ru>
2024-01-14 02:23:53 +0300

    RegisterExistingSongs: actualized technical script to work with new storage

--- src/app/Console/Commands/RegisterExistingSongs.php
+++ src/app/Console/Commands/RegisterExistingSongs.php
@@ -20,10 +20,10 @@ class RegisterExistingSongs extends Command
     {
         echo 'The script is disabled', PHP_EOL;
         exit;
-        $input_fs = Storage::disk(StorageName::PUBLIC_STORAGE->value);
-        $path = $input_fs->path('/tmp/');
+        $inputDisk = Storage::disk(StorageName::PUBLIC_STORAGE->value);
+        $inputPath = $inputDisk->path('/previous-storage/');
 
-        $wrong_counts = [];
+        $wrongCounts = [];
         $songs = Song::all();
         /** @var Song $song */
         foreach ($songs as $song) {
@@ -35,7 +35,7 @@ class RegisterExistingSongs extends Command
             $source_count = count($source_data);
             $source = $source_data[array_key_last($source_data)];
             if (1 !== $source_count) {
-                $wrong_counts[] = [
+                $wrongCounts[] = [
                     'title' => $song->title,
                     'source' => $source_data,
                 ];
@@ -43,19 +43,27 @@ class RegisterExistingSongs extends Command
                     continue;
                 }
             }
-            $input_path = $path.$source->download_link;
-            if (!is_file($input_path)) {
+
+            $sourceFilePath = $inputPath.$source->download_link;
+            if (!is_file($sourceFilePath)) {
                 throw new LogicException('No file for '.$song->title);
             }
 
-            $file = new UploadedFile($input_path, $source->original_name);
-            $attachment = (new File($file))->path('/public/songs/'.date('Y-m-d', filemtime($input_path)).'/')->load();
+            $outputRelativeDir = '/songs/'.date('Y-m-d', filemtime($sourceFilePath)).'/';
+
+            $file = new UploadedFile($sourceFilePath, $source->original_name);
+
+            $attachment = (new File($file, StorageName::RADIO_STORAGE->value))
+                ->path($outputRelativeDir)
+                ->load();
+
             $song->song_attachment_id = $attachment->id;
             $song->save();
+
             echo $song->title, PHP_EOL;
         }
         echo '------', PHP_EOL;
-        var_dump($wrong_counts);
+        var_dump($wrongCounts);
         echo 'Done', PHP_EOL;
 
         return Command::SUCCESS;
