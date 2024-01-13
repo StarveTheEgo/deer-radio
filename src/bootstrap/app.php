006aa9523ecf6d8c0a2850767918e61ee0489aa3
StarveTheEgo <alone_monax@xakep.ru>
2024-01-13 22:00:38 +0300

    Fixed env file loading

--- src/bootstrap/app.php
+++ src/bootstrap/app.php
@@ -15,7 +15,7 @@ $app = new Illuminate\Foundation\Application(
     $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
 );
 
-$app->useEnvironmentPath('../.env');
+$app->loadEnvironmentFrom('../.env');
 
 /*
 |--------------------------------------------------------------------------
