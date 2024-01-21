7ede0fc5c68853782b16e5c4a01d2c818abaaa24
StarveTheEgo <alone_monax@xakep.ru>
2024-01-21 06:19:25 +0300

    account-button.blade - Added  reconnect button; Simplified the code;

--- src/app/Components/ServiceAccount/resources/views/account-button.blade.php
+++ src/app/Components/ServiceAccount/resources/views/account-button.blade.php
@@ -10,6 +10,23 @@
 
     /** @var ServiceAccount $serviceAccount */
     $accessToken = $serviceAccount->getAccessToken();
+    $tokenExists = ($accessToken !== null);
+
+    $isExpired = false;
+    if ($tokenExists) {
+        $expirationDate = $accessToken->getExpiresAt();
+        if ($expirationDate !== null) {
+            $currentDate = new DateTimeImmutable();
+            $isExpired = ($expirationDate <= $currentDate);
+        }
+    }
+
+    $showConnectButton = (!$tokenExists || $isExpired);
+    $connectButtonText = $tokenExists ? 'Reconnect' : 'Connect';
+
+    $serviceAccountId = $serviceAccount->getId();
+    $connectRoute = route(ServiceAccountRoute::OAUTH_REDIRECT->value, ['serviceAccount' => $serviceAccountId]);
+    $disconnectRoute = route(ServiceAccountRoute::OAUTH_DISCONNECT->value, ['serviceAccount' => $serviceAccountId]);
 
     $tokenInfoViewName = sprintf('%s::access-token-info', ServiceAccountServiceProvider::RESOURCE_NS);
 @endphp
@@ -17,17 +34,11 @@
 @include($tokenInfoViewName)
 
 <div class="form-group">
-    @if ($accessToken === null)
-        <a class="btn btn-primary btn-lg"
-           href="{{ route(ServiceAccountRoute::OAUTH_REDIRECT->value, ['serviceAccount' => $serviceAccount->getId()]) }}"
-           role="button">
-            Connect
-        </a>
-    @else
-        <a class="btn btn-primary btn-lg"
-           href="{{ route(ServiceAccountRoute::OAUTH_DISCONNECT->value, ['serviceAccount' => $serviceAccount->getId()]) }}"
-           role="button">
-            Disconnect
-        </a>
+    @if ($showConnectButton)
+        <a class="btn btn-primary btn-lg" href="{{ $connectRoute }}" role="button">{{ $connectButtonText }}</a>
+    @endif
+
+    @if ($tokenExists)
+        <a class="btn btn-danger btn-lg" href="{{ $disconnectRoute }}" role="button">Disconnect</a>
     @endif
 </div>
