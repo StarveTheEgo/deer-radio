a1d248fc18e94159e7208d8cd934a3793b997918
StarveTheEgo <alone_monax@xakep.ru>
2024-01-22 06:57:16 +0300

    Added 'access-token:refresh' command to a scheduler

--- src/app/Console/Kernel.php
+++ src/app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+         $schedule->command('access-token:refresh')->everyMinute();
     }
 
     /**
