f552b4e74aadff5347124a2be00d196a75889904
StarveTheEgo <alone_monax@xakep.ru>
2024-02-06 23:37:38 +0300

    Actualized Deer Radio enums; Added DeerRadioSettingKey;

---
+++ src/app/Components/DeerRadio/Enum/DeerRadioSettingKey.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Components\DeerRadio\Enum;
+
+enum DeerRadioSettingKey: string
+{
+    case TITLE = 'deer-radio.title';
+
+    case DESCRIPTION = 'deer-radio.description';
+}
