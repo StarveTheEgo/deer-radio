a19ae34f66add450cfcbe8c563fb9303bbce63eb
StarveTheEgo <alone_monax@xakep.ru>
2024-02-07 21:47:51 +0300

    DatabaseSeeder - removed unused class

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -20,7 +20,6 @@ class DatabaseSeeder extends Seeder
             UnsplashClientSettingSeeder::class,
             UnsplashQuerySettingSeeder::class,
             DeerRadioSettingSeeder::class,
-            GoogleOutputSettingSeeder::class,
         ]);
     }
 }
