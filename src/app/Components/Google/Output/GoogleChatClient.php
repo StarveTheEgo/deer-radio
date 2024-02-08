490a1876dcea26560db6d144e85ccb4d428cbb4a
StarveTheEgo <alone_monax@xakep.ru>
2024-02-09 01:22:50 +0300

    GoogleChatClient - migrated message splitting feature

--- src/app/Components/Google/Output/GoogleChatClient.php
+++ src/app/Components/Google/Output/GoogleChatClient.php
@@ -20,6 +20,12 @@ use Webmozart\Assert\Assert;
 
 class GoogleChatClient implements ChatClientInterface
 {
+    /** @var int Limit of characters per single LiveChatMessage */
+    public const MESSAGE_LENGTH_LIMIT = 180;
+
+    /** @var string Delimiter between message parts */
+    private const MESSAGE_PARTS_DELIMITER = ' ... ';
+
     /** @var GoogleDataAccessor */
     private GoogleDataAccessor $dataAccessor;
 
@@ -69,8 +75,69 @@ class GoogleChatClient implements ChatClientInterface
 
         $youtubeApi = $this->youtubeApiFactory->getOrCreateForOutput($output);
 
-        $liveChatMessage = $this->buildLiveChatMessage($liveBroadcast, $messageText);
-        $youtubeApi->createLiveChatMessage($liveChatMessage);
+        $formattedMessages = $this->splitIntoFormattedChatMessages($messageText);
+        Assert::minCount($formattedMessages, 1);
+
+        foreach ($formattedMessages as $formattedMessage) {
+            $liveChatMessage = $this->buildLiveChatMessage($liveBroadcast, $formattedMessage);
+            $youtubeApi->createLiveChatMessage($liveChatMessage);
+        }
+    }
+
+    /**
+     * Splits specified message into list of formatted YouTube LiveChatMessages
+     * @param string $message
+     * @return array<string>
+     */
+    public function splitIntoFormattedChatMessages(string $message) : array
+    {
+        $message = str_replace("\n", '. ', $message);
+
+        $lengthLimit = self::MESSAGE_LENGTH_LIMIT;
+        $delimiter = self::MESSAGE_PARTS_DELIMITER;
+
+        $extraLength = $lengthLimit - mb_strlen($message);
+        if ($extraLength > 0) {
+            // the text length exceeds the limit, let's split it into strings of specified limit
+            $wordWrappedText = $this->multibyteWordWrap($message, $lengthLimit);
+            // split word wrapped text by lines
+            $messageParts = array_filter(explode("\n", $wordWrappedText));
+        } else {
+            $messageParts = [$message];
+        }
+
+        // add visual delimiters between parts
+        $partsCount = count($messageParts);
+        for ($i = 0; $i < $partsCount; $i++) {
+            if ($i === 0 && $partsCount > 1) {
+                // this is the first part with upcoming parts
+                $messageParts[$i] .= $delimiter;
+            } elseif ($i > 0) {
+                // any next part should be prefixed by delimiter as well
+                $messageParts[$i] = $delimiter.$messageParts[$i];
+            }
+        }
+
+        return $messageParts;
+    }
+
+    /**
+     * @param string $string
+     * @param int $width
+     * @param string $break
+     * @return string
+     */
+    private function multibyteWordWrap(string $string, int $width = 75, string $break = "\n"): string
+    {
+        // Match anything 1 to $width chars long followed by whitespace or EOS,
+        // otherwise match anything $width chars long
+        $search = '/(.{1,'.$width.'})(?:\s|$)|(.{'.$width.'})/uS';
+        $replace = '$1$2'.$break;
+
+        /** @var string $wordWrappedText */
+        $wordWrappedText = preg_replace($search, $replace, $string);
+
+        return $wordWrappedText;
     }
 
     /**
