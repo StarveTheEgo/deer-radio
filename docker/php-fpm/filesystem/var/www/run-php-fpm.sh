f6ad5da24f943885787b982d8bedecca1ef9a463
StarveTheEgo <alone_monax@xakep.ru>
2024-02-13 04:08:59 +0300

    Docker, php-fpm - added the call of 'access-token:refresh' in the entrypoint

--- docker/php-fpm/filesystem/var/www/run-php-fpm.sh
+++ docker/php-fpm/filesystem/var/www/run-php-fpm.sh
@@ -5,5 +5,6 @@ set -e
 php /var/www/app/artisan migrate --seed
 php /var/www/app/artisan liquidsoap:user --if-not-exists
 php /var/www/app/artisan liquidsoap:personal-token
+php /var/www/app/artisan access-token:refresh
 
 php-fpm -F
