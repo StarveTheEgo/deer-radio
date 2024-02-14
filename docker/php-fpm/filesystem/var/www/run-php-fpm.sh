19397d4e33bd6fe7fd7303d3746aef3777fde0bb
StarveTheEgo <alone_monax@xakep.ru>
2024-02-14 21:14:32 +0300

    Docker, php-fpm - added Laravel key generation (if does not exist)

--- docker/php-fpm/filesystem/var/www/run-php-fpm.sh
+++ docker/php-fpm/filesystem/var/www/run-php-fpm.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+# Create key if necessary
+if ! grep -q APP_KEY .env 2>/dev/null ; then
+  php artisan key:generate --force
+fi
 php /var/www/app/artisan migrate --seed
 php /var/www/app/artisan liquidsoap:user --if-not-exists
 php /var/www/app/artisan liquidsoap:personal-token
