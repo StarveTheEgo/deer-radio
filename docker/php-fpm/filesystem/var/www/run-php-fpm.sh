aac1c08220524f31372a1761bf40a2e2d1234519
StarveTheEgo <alone_monax@xakep.ru>
2024-02-15 06:34:30 +0300

    Docker, php-fpm - added '--force' option for migrate command

--- docker/php-fpm/filesystem/var/www/run-php-fpm.sh
+++ docker/php-fpm/filesystem/var/www/run-php-fpm.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-php /var/www/app/artisan migrate --seed
+php /var/www/app/artisan migrate --seed --force
 php /var/www/app/artisan liquidsoap:user --if-not-exists
 php /var/www/app/artisan liquidsoap:personal-token
 php /var/www/app/artisan access-token:refresh
