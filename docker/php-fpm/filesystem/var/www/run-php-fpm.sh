77c75ddf0ac00494781cf46f110b3f3cf800410a
StarveTheEgo <alone_monax@xakep.ru>
2024-02-15 05:22:34 +0300

    Docker, php-fpm - fixed app key generation (condition was incorrect); --force option was removed

--- docker/php-fpm/filesystem/var/www/run-php-fpm.sh
+++ docker/php-fpm/filesystem/var/www/run-php-fpm.sh
@@ -2,10 +2,7 @@
 
 set -e
 
-# Create key if necessary
-if ! grep -q APP_KEY .env 2>/dev/null ; then
-  php artisan key:generate --force
-fi
+php /var/www/app/artisan key:generate
 php /var/www/app/artisan migrate --seed
 php /var/www/app/artisan liquidsoap:user --if-not-exists
 php /var/www/app/artisan liquidsoap:personal-token
