7d9b725bb691dbf4c52352eb0e7a2c47faf96187
StarveTheEgo <alone_monax@xakep.ru>
2024-02-18 05:10:11 +0300

    Fixed update-sources.sh (added dots to specify that we want to copy directory contents

--- update-sources.sh
+++ update-sources.sh
@@ -3,7 +3,7 @@
 # This is a handy script that you will probably never use
 
 # Update only app subdirectory of Laravel app (we want to exclude big vendor directory, but later someday)
-docker cp ./src/app/ deer-radio-php-fpm:/var/www/app/app/
+docker cp ./src/app/. deer-radio-php-fpm:/var/www/app/app/
 
 # Update entire liquidsoap app
-docker cp ./docker/liquidsoap/radio/ deer-radio-liquidsoap:/var/liquidsoap/radio
+docker cp ./docker/liquidsoap/radio/. deer-radio-liquidsoap:/var/liquidsoap/radio
