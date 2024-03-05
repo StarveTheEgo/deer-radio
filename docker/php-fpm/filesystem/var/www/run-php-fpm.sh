85a3107ff456238d2a2f135341261f7aaf37eeb3
StarveTheEgo <alone_monax@xakep.ru>
2024-03-06 00:50:03 +0300

    Docker, php-fpm - removed access-token refreshing from the entrypoint script

--- docker/php-fpm/filesystem/var/www/run-php-fpm.sh
+++ docker/php-fpm/filesystem/var/www/run-php-fpm.sh
@@ -5,6 +5,5 @@ set -e
 php /var/www/app/artisan migrate --seed --force
 php /var/www/app/artisan liquidsoap:user --if-not-exists
 php /var/www/app/artisan liquidsoap:personal-token
-php /var/www/app/artisan access-token:refresh
 
 php-fpm -F
