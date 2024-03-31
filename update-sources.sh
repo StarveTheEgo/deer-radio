c039e9a5f75c1bcceb68a9fee2b7ec0829fcd538
StarveTheEgo <alone_monax@xakep.ru>
2024-03-31 19:59:19 +0300

    update-sources.sh - added config/ and database/ directories for sync

--- update-sources.sh
+++ update-sources.sh
@@ -4,6 +4,8 @@
 
 # Update only app subdirectory of Laravel app (we want to exclude big vendor directory, but later someday)
 docker cp ./src/app/. deer-radio-php-fpm:/var/www/app/app/
+docker cp ./src/config/. deer-radio-php-fpm:/var/www/app/config/
+docker cp ./src/database/. deer-radio-php-fpm:/var/www/app/database/
 
 # Update entire liquidsoap app
 docker cp ./docker/liquidsoap/radio/. deer-radio-liquidsoap:/var/liquidsoap/radio
