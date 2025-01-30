57fc8b09cdd7bc8a1045903ad346459ebf28a863
StarveTheEgo <alone_monax@xakep.ru>
2025-01-30 04:32:19 +0300

    Actualized handy dev script (update-sources.sh)

--- update-sources.sh
+++ update-sources.sh
@@ -3,7 +3,9 @@
 # This is a handy script that you will probably never use
 
 # Update only app subdirectory of Laravel app (we want to exclude big vendor directory, but later someday)
+docker cp ./.env deer-radio-php-fpm:/var/www/.env
 docker cp ./src/app/. deer-radio-php-fpm:/var/www/app/app/
+docker cp ./src/resources/. deer-radio-php-fpm:/var/www/app/resources/
 docker cp ./src/config/. deer-radio-php-fpm:/var/www/app/config/
 docker cp ./src/database/. deer-radio-php-fpm:/var/www/app/database/
 
