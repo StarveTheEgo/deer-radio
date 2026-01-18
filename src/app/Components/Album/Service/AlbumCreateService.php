54c8261984d08296073d48293e818a1b948fcd81
StarveTheEgo <alone_monax@xakep.ru>
2026-01-18 22:29:12 +0300

    Album component - upgrade to php 8.4

--- src/app/Components/Album/Service/AlbumCreateService.php
+++ src/app/Components/Album/Service/AlbumCreateService.php
@@ -9,11 +9,8 @@ use App\Components\Album\Repository\AlbumRepositoryInterface;
 
 class AlbumCreateService
 {
-    private AlbumRepositoryInterface $repository;
-
-    public function __construct(AlbumRepositoryInterface $repository)
+    public function __construct(private readonly AlbumRepositoryInterface $repository)
     {
-        $this->repository = $repository;
     }
 
     public function create(Album $album): void
--- src/app/Components/Album/Service/AlbumDeleteService.php
+++ src/app/Components/Album/Service/AlbumDeleteService.php
@@ -9,11 +9,8 @@ use App\Components\Album\Repository\AlbumRepositoryInterface;
 
 class AlbumDeleteService
 {
-    private AlbumRepositoryInterface $repository;
-
-    public function __construct(AlbumRepositoryInterface $repository)
+    public function __construct(private readonly AlbumRepositoryInterface $repository)
     {
-        $this->repository = $repository;
     }
 
     public function delete(Album $album): void
