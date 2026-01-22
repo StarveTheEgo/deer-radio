474c1a53f5fc3a9d5cd1f0c8367b225ab85b43b0
StarveTheEgo <evergreendeer@deer-radio.net>
2026-01-22 03:04:46 +0300

    Remove obsolete resolver class (routing was fixed by orchid devs)

--- src/app/Orchid/Screens/AbstractScreen.php
+++ src/app/Orchid/Screens/AbstractScreen.php
@@ -5,10 +5,7 @@ declare(strict_types=1);
 namespace App\Orchid\Screens;
 
 use App\Components\DoctrineOrchid\Filter\AbstractDoctrineFilter;
-use App\Components\OrchidIntergration\Resolvers\DoctrineAwareScreenDependencyResolver;
-use Illuminate\Contracts\Container\BindingResolutionException;
 use Orchid\Screen\Screen;
-use ReflectionException;
 
 abstract class AbstractScreen extends Screen
 {
@@ -44,22 +41,6 @@ abstract class AbstractScreen extends Screen
     public function permission(): ?iterable
     {
         return static::getPermissions();
-    }
-
-    /**
-     * @param string $method
-     * @param array<string, mixed>  $httpQueryArguments
-     *
-     * @throws BindingResolutionException
-     * @throws ReflectionException
-     *
-     * @return array<string, mixed>
-     */
-    protected function resolveDependencies(string $method, array $httpQueryArguments = []): array
-    {
-        /** @var DoctrineAwareScreenDependencyResolver $resolver */
-        $resolver = app()->make(DoctrineAwareScreenDependencyResolver::class);
-        return $resolver->resolveScreen($this, $method, $httpQueryArguments);
     }
 
     /**
